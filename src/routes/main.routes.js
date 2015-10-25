6761bd52f3aaf9ec39e5e50f8678a9e6acdf71e2
adam klein <adamklein.mail@gmail.com>
2015-10-25 21:32:19 +0000

    Update main.routes.js

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,9 +1,10 @@
+// Import node module
 import express from 'express'
 const router = express.Router();
 
-// middleware specific to this router
-// define the home page route
+// Arrow functions
 router.get('/', (req, res) => {
   res.send({message: 'Hello World!'});
 });
+// Exporting an object as the default import for this module
 export default router;

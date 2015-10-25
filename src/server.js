55eded3cdb0aa26c484ebb6ce689e79e2a135a72
adam klein <adamklein.mail@gmail.com>
2015-10-25 21:31:16 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
+// Importing node modules
 import express from 'express'
+// Importing source files
 import routes from './routes/main.routes'
+// consts
 const app = express();
 
 app.use('/', routes);
 
-var server = app.listen(3000, () => {
-  var host = server.address().address;
-  var port = server.address().port;
+// arrow functions
+app.listen(3000, () => {
+  const host = server.address().address;
+  const port = server.address().port;
 
+  // string interpolation:
   console.log(`Example app listening at http://${host}:${port}`);
 });
